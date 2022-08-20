c42629861a14dec306beda4f3099c341d1c2bf33
hackyannick <hack.yannick@gmail.com>
2022-08-20 15:04:47 +0200

    Update prepare.sh

--- dc1/prepare.sh
+++ dc1/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update && apt upgrade -y
+apt update 
+apt upgrade -y
 apt install wget sudo nano -y
 wget https://raw.githubusercontent.com/hackyannick/samba4-dc/main/dc1/interfaces.sav
 rm -rf /etc/network/interfaces
