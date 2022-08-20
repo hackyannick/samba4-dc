4c90a0733346cb098dda2a2ac3d048de511bd7a2
hackyannick <hack.yannick@gmail.com>
2022-08-20 15:02:37 +0200

    Update setup.sh

--- dc1/setup.sh
+++ dc1/setup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+echo Bitte Domain eingeben (DNS):
+read realmdn
+echo Bitte kurze Domain abgeben:
+read realmshort
+echo Bitte Administratorpasswort eingeben:
+read adminpass
 wget https://raw.githubusercontent.com/hackyannick/samba4-dc/main/dc1/resolv.sav
 apt install samba smbclient winbind krb5-user krb5-config krb5-locales winbind libpam-winbind libnss-winbind -y
 mkdir /opt/smb_dc
@@ -7,7 +14,7 @@ systemctl disable samba-ad-dc.service smbd.service nmbd.service winbind.service
 smbd -b | grep "CONFIGFILE"
 mv /etc/samba/smb.conf /etc/samba/smb.conf.backup
 rm -rf /etc/krb5.conf
-samba-tool domain provision --server-role=dc --use-rfc2307 --dns-backend=SAMBA_INTERNAL --realm=AD.HACKDV.COM --domain=AD --adminpass=7SYgdBcYH96M4r
+samba-tool domain provision --server-role=dc --use-rfc2307 --dns-backend=SAMBA_INTERNAL --realm=$realmdn --domain=$realmshort --adminpass=$adminpass
 mv /etc/krb5.conf /etc/krb5.conf.backup
 ln -sf /var/lib/samba/private/krb5.conf /etc/krb5.conf
 systemctl unmask samba-ad-dc.service
