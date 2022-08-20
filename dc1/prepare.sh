2868d9c0915bcb7619a6b79562d4ab27faca0f66
hackyannick <hack.yannick@gmail.com>
2022-08-20 14:50:18 +0200

    Add files via upload

---
+++ dc1/prepare.sh
@@ -0,0 +1,6 @@
+apt update && apt upgrade -y
+apt install wget sudo nano -y
+wget https://raw.githubusercontent.com/hackyannick/samba4-dc/main/dc1/interfaces.sav
+rm -rf /etc/network/interfaces
+cp /opt/smb_dc/interfaces.sav /etc/network/interfaces
+reboot
